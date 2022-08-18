c065ff18465e2af7fd5506eebb79ee87208516f0
dani232g <111403378+dani232g@users.noreply.github.com>
2022-08-17 19:53:36 -0600

    From IFs to Match

--- password01.py
+++ password01.py
@@ -1,7 +1,4 @@
 import random
-password = ''
-pass_length = int(input('Enter the desired password length: '))
-
 
 def pass_generation(length):
     pass_components = []
@@ -10,21 +7,27 @@ def pass_generation(length):
     pass_components.append(chr(random.randint(97, 112))) #Lowercase
     pass_components.append(chr(random.randint(33, 41)))  #Symbol
     pass_components.append(chr(random.randint(48, 57)))  #Number
-
     # Fill the rest of the list with random components
     for i in range(length - 4):
         character = random.randint(1 , 4)
-        if (character == 1):
-            pass_components.append(chr(random.randint(65, 90)))  #Uppercase
-        elif (character == 2):
-            pass_components.append(chr(random.randint(97, 112))) #Lowercase
-        elif (character == 3):
-            pass_components.append(chr(random.randint(33, 41)))  #Symbol
-        else:
-            pass_components.append(chr(random.randint(48, 57)))  #Number
+        match character:
+            case 1 :
+                pass_components.append(chr(random.randint(65, 90)))  #Uppercase
+            case 2:
+                pass_components.append(chr(random.randint(97, 112))) #Lowercase
+            case 3:
+                pass_components.append(chr(random.randint(33, 41)))  #Symbol
+            case 4:
+                pass_components.append(chr(random.randint(48, 57)))  #Number
+                
     random.shuffle(pass_components)
     return pass_components
 
-password = ''.join(pass_generation(pass_length))
-print (password)
-print (len(password))
+def main():
+    password = ''
+    pass_length = int(input('Enter the desired password length: '))
+    password = ''.join(pass_generation(pass_length))
+    print (password)
+    print (len(password))
+
+main()
